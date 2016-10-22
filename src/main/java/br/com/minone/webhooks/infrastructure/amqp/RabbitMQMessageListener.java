972ace748759158742a46a3e3773197fe668ce7e
Manoel Menezes <manoel.menezes.jr@gmail.com>
2016-10-22 18:43:45 -0300

    Validating the 24 hours.

--- src/main/java/br/com/minone/webhooks/infrastructure/amqp/RabbitMQMessageListener.java
+++ src/main/java/br/com/minone/webhooks/infrastructure/amqp/RabbitMQMessageListener.java
@@ -1,5 +1,7 @@
 package br.com.minone.webhooks.infrastructure.amqp;
 
+import java.util.Date;
+
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 
@@ -9,6 +11,10 @@ import br.com.minone.webhooks.infrastructure.service.IncredibleHookService;
 
 public class RabbitMQMessageListener implements ChannelAwareMessageListener {
 
+	private static final int HOURS = 2;
+
+	private static final long DAY_MILISECONDS = 60000L;
+
 	private final IncredibleHookService incredibleHookService;
 
 	public RabbitMQMessageListener() {
@@ -17,17 +23,27 @@ public class RabbitMQMessageListener implements ChannelAwareMessageListener {
 
 	@Override
 	public void onMessage(Message message, Channel channel) throws Exception {
-		
+
 		String url = message.getMessageProperties().getReplyTo();
-		
+
 		String content = new String(message.getBody());
-		
+
 		String contentType = message.getMessageProperties().getContentType();
-		
+
 		boolean success = incredibleHookService.deliver(url, content, contentType);
-		
+
 		if (!success) {
-			channel.basicReject(message.getMessageProperties().getDeliveryTag(), true);
+
+			Date messageDate = message.getMessageProperties().getTimestamp();
+
+			Date now = new Date();
+
+			long diffInHours = (now.getTime() - messageDate.getTime()) / DAY_MILISECONDS;
+
+			if (diffInHours < HOURS) {
+				channel.basicReject(message.getMessageProperties().getDeliveryTag(), true);
+			}
+
 		}
 
 	}
