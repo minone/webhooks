a10e005bb1c4de813c51de2678b005e6b930d9f2
Manoel Menezes <manoel.menezes.jr@gmail.com>
2016-10-21 22:13:22 -0300

    classes RabbitMQ

---
+++ src/main/java/br/com/minone/webhooks/infrastructure/amqp/RabbitMQMessageListener.java
@@ -0,0 +1,14 @@
+package br.com.minone.webhooks.infrastructure.amqp;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.transaction.annotation.Transactional;
+
+public class RabbitMQMessageListener implements MessageListener {
+
+	@Transactional
+	@Override
+	public void onMessage(Message message) {
+
+	}
+}
