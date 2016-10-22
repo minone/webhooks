f0d9f1f7582c36535a89cc15374e9accf6d6ad48
Felipe Menezes Cardoso <felipe.computacao@gmail.com>
2016-10-22 16:07:29 -0300

    database script updated with constraint

--- assets/database-script.sql
+++ assets/database-script.sql
@@ -5,3 +5,7 @@ CREATE TABLE destination (
   dest_tx_url         VARCHAR(2000) NOT NULL,
   dest_tx_secret      VARCHAR(36)   NOT NULL
 );
+
+ALTER TABLE destination ADD CONSTRAINT pk_dest PRIMARY KEY (dest_id_destination);
+ALTER TABLE destination ADD CONSTRAINT uk_dest_tx_url UNIQUE (dest_tx_url);
+ALTER TABLE destination ADD CONSTRAINT uk_dest_tx_secret UNIQUE (dest_tx_secret);
