d37cfc5bf399c4230547fb5b33500b0009bce729
danielfmsouza <danielfmsouza@gmail.com>
2016-10-21 22:36:22 -0300

    postMessage and repository for Destination

--- src/main/java/br/com/minone/webhooks/application/DestinationApplicationService.java
+++ src/main/java/br/com/minone/webhooks/application/DestinationApplicationService.java
@@ -2,10 +2,17 @@ package br.com.minone.webhooks.application;
 
 
 import br.com.minone.webhooks.application.command.RegisterDestinationCmd;
+import br.com.minone.webhooks.domain.model.Destination;
+import br.com.minone.webhooks.domain.model.DestinationId;
+
+import java.util.UUID;
 
 public class DestinationApplicationService {
 
     public void registerDestination(RegisterDestinationCmd cmd) {
+        DestinationId destinationId = new DestinationId(UUID.randomUUID());
+
+        Destination destination = new Destination(destinationId, cmd.getURL());
     }
 
     public void deleteDestination(String destinationId) {
--- src/main/java/br/com/minone/webhooks/domain/model/DestinationRepository.java
+++ src/main/java/br/com/minone/webhooks/domain/model/DestinationRepository.java
@@ -4,4 +4,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DestinationRepository {
+
+    void registerDestination(Destination destination);
+
+    void updateDestination(Destination destination);
+
+    void deleteDestination(DestinationId destinationId);
+
+    Destination loadDestination(DestinationId destinationId);
 }
